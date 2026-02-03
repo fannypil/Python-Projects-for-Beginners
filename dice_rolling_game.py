d8b346f63b1548ef238296f04b1a7b8e5b57eced
fannypil <fanysh3@gmail.com>
2026-02-03 17:29:34 +0200

    Modified so user can specify how many dice they want to roll

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -7,9 +7,18 @@ def main():
     while True:
         choice = input("Roll the dice? (y/n): ").strip().lower()
         if choice == 'y':
-            result1 = roll_dice()
-            result2 = roll_dice()
-            print(f"({result1}, {result2})")
+            n = input("How many dice to roll?").strip()
+            if n.isdigit() and int(n)>0:
+                n=int(n)
+                results  = []
+                for _ in range(n):
+                    results.append(roll_dice())
+                if n==1:
+                    print(f"({results[0]})")
+                else:
+                    print(tuple(results))
+            else:
+                print("Please enter a valid positive integer.")
         elif choice == 'n':
             print("Thanks for playing!")
             break
